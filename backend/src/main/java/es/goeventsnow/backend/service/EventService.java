2d5e471be39fa58d3a0f3576e8f6774190db0386
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2025-12-11 18:49:30 +0100

    fix: Remove @Autowired annotation and implement constructor injection for EventRepository feature: add event functionality

--- backend/src/main/java/es/goeventsnow/backend/service/EventService.java
+++ backend/src/main/java/es/goeventsnow/backend/service/EventService.java
@@ -2,7 +2,6 @@ package es.goeventsnow.backend.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import es.goeventsnow.backend.model.Event;
@@ -11,13 +10,19 @@ import es.goeventsnow.backend.repository.EventRepository;
 @Service
 public class EventService {
     
-    @Autowired
-    private EventRepository eventRepository;
+    private final EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository){
+        this.eventRepository = eventRepository;
+    }
 
     public List<Event> getAllEvents(){
        return eventRepository.findAll();
     }
 
+    public Event addEvent(Event event){
+        return eventRepository.save(event);
+    }
 
     
 }

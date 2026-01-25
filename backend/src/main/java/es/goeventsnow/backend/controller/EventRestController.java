72807fb9df7b38a093959d1b60a8e75ffa3b8c1e
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2026-01-25 12:11:05 +0100

    Implement CORS configuration and remove unused EventController and index.html

--- backend/src/main/java/es/goeventsnow/backend/controller/EventRestController.java
+++ backend/src/main/java/es/goeventsnow/backend/controller/EventRestController.java
@@ -5,19 +5,18 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import es.goeventsnow.backend.model.Event;
 import es.goeventsnow.backend.service.EventService;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import es.goeventsnow.backend.model.Event;
-
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/events")
 public class EventRestController {

b5afced5d990f3e3bf570e645d02d9dc36691230
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2026-03-12 12:04:54 +0100

    Feature: Implement JWT-based authentication and authorization

---
+++ backend/src/main/java/es/goeventsnow/backend/security/SecurityConfiguration.java
@@ -0,0 +1,88 @@
+package es.goeventsnow.backend.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import es.goeventsnow.backend.security.jwt.JwtRequestFilter;
+import es.goeventsnow.backend.security.jwt.UnauthorizedHandlerJwt;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    RepositoryUserDetailsService userDetailService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    private UnauthorizedHandlerJwt unauthorizedHandlerJwt;
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authenticationProvider(authenticationProvider());
+
+        http
+				.securityMatcher("/api/v1/**")
+				.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+
+         http.authorizeHttpRequests( authorize -> authorize
+
+            .requestMatchers(HttpMethod.POST,"/api/v1/events/**").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.PUT,"/api/v1/events/**").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.DELETE,"/api/v1/events/**").hasRole("ADMIN")
+
+            .requestMatchers(HttpMethod.POST,"/api/v1/participants/**").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.PUT,"/api/v1/participants/**").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.DELETE,"/api/v1/participants/**").hasRole("ADMIN")
+          
+            .anyRequest().permitAll()
+        );
+
+        http.formLogin(formLogin -> formLogin.disable());
+
+        http.csrf(csrf -> csrf.disable());
+
+        http.httpBasic(httpBasic -> httpBasic.disable());
+
+        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        
+        return http.build();
+        
+    }
+}

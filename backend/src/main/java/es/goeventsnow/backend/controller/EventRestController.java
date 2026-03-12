c458da325947c1240034c75f7fc0b3172da34ef0
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2026-03-12 13:05:22 +0100

    Feature: Implement Event management with REST controller, service, repository, DTO, and mapper

--- backend/src/main/java/es/goeventsnow/backend/controller/EventRestController.java
+++ backend/src/main/java/es/goeventsnow/backend/controller/EventRestController.java
@@ -1,22 +1,33 @@
 package es.goeventsnow.backend.controller;
 
+import java.io.IOException;
 import java.net.URI;
+import java.sql.SQLException;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
-import es.goeventsnow.backend.model.Event;
+import es.goeventsnow.backend.dto.event.EventDTO;
 import es.goeventsnow.backend.service.EventService;
 
-@CrossOrigin(origins = "*")
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 @RestController
 @RequestMapping("/api/v1/events")
 public class EventRestController {
@@ -25,22 +36,66 @@ public class EventRestController {
     private EventService eventService;
 
     @GetMapping("/")
-    public Collection<Event> getEvents() {
+    public Collection<EventDTO> getEvents() {
         return eventService.getAllEvents();
     }
 
+    @GetMapping("/{id}")
+    public EventDTO getEventById(@PathVariable long id) {
+        return eventService.getEventById(id);
+    }
+
     @PostMapping("/")
-    public ResponseEntity<Event> postEvent(@RequestBody Event event) {
-        Event savedEvent = eventService.addEvent(event);
+    public ResponseEntity<EventDTO> postEvent(@RequestBody EventDTO eventDTO) {
+        EventDTO savedEventDTO = eventService.addEvent(eventDTO);
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
-                .buildAndExpand(savedEvent.getId())
+                .buildAndExpand(savedEventDTO.id())
                 .toUri();
 
-        return ResponseEntity.created(location).body(savedEvent);
+        return ResponseEntity.created(location).body(savedEventDTO);
     }
-    
+
+    @DeleteMapping("/{id}")
+    public EventDTO deleteEvent(@PathVariable long id){
+        return eventService.deleteEvent(id);
+    }
+
+    @PutMapping("/{id}")
+    public EventDTO replaceEvent(@PathVariable long id, @RequestBody EventDTO eventDTO) throws SQLException {
+        return eventService.replaceEvent(id, eventDTO);
+    }
+
+    @PostMapping("/{id}/image")
+    public ResponseEntity<Object> createEventImage(@PathVariable long id, @RequestParam MultipartFile imageFile) throws IOException {
+
+        URI location = fromCurrentRequest().build().toUri();
+        eventService.createEventImage(id, imageFile.getInputStream(), imageFile.getSize());
+        return ResponseEntity.created(location).build();
+   
+    }
+
+    @GetMapping("/{id}/image")
+    public ResponseEntity<Object> getEventImage(@PathVariable long id) throws IOException, SQLException {
+
+        Resource eventImage = eventService.getEventImage(id);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE,"image/jpeg").body(eventImage);
+    }
+
+    @PutMapping("/{id}/image")
+    public ResponseEntity<Object> replaceEventImage(@PathVariable long id, @RequestParam MultipartFile imageFile) throws IOException {
+
+        eventService.replaceEventImage(id, imageFile.getInputStream(), imageFile.getSize());
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}/image")
+    public ResponseEntity<Object> deleteEventImage(@PathVariable long id) {
+
+        eventService.deleteEventImage(id);
+        return ResponseEntity.noContent().build();
+    }
     
 }
--- backend/src/main/java/es/goeventsnow/backend/model/Event.java
+++ backend/src/main/java/es/goeventsnow/backend/model/Event.java
@@ -1,11 +1,22 @@
 package es.goeventsnow.backend.model;
 
+import java.sql.Blob;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "events")
 public class Event {
 
     @Id
@@ -17,14 +28,28 @@ public class Event {
     private String location;
     private String date;
 
+    private boolean image;
+
+    @Lob 
+    @JsonIgnore
+    private Blob imageFile;
+
+    @ManyToMany
+    private List<Participant> participants;
+
+    @OneToMany(mappedBy = "event") 
+    private List<Ticket> tickets;
+
+
     public Event(){}
 
-    public Event (String title, String category, String location, String date){
+    public Event (String title, String category, String location, String date, List<Participant> participants) {
         super();
         this.title=title;
         this.category=category;
         this.location=location;
         this.date=date;
+        this.participants=participants;
     }
 
     public Long getId() {
@@ -67,5 +92,36 @@ public class Event {
         this.date = date;
     }
 
+    public boolean getImage() {
+        return image;
+    }
+
+    public void setImage(boolean image) {
+        this.image = image;
+    }
+
+    public Blob getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(Blob imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    public List<Participant> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<Participant> participants) {
+        this.participants = participants;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 
 }

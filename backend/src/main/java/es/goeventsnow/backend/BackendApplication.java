3f1310da4c591c95bb83a1ee9bb891cb20cf421a
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2026-03-13 11:33:21 +0100

    Feature: Implement backend pagination for event retrieval & change the tests in order to work

--- backend/src/main/java/es/goeventsnow/backend/BackendApplication.java
+++ backend/src/main/java/es/goeventsnow/backend/BackendApplication.java
@@ -2,8 +2,14 @@ package es.goeventsnow.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EntityScan("es.goeventsnow.backend.model")
+@EnableSpringDataWebSupport(
+	pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
 public class BackendApplication {
 
 	public static void main(String[] args) {

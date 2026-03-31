f41bb058d4acd2b6f5fdb0f214a93da4e20c06be
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2026-03-31 12:34:50 +0200

    refactor: remove unused router imports from main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,13 +5,11 @@ import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app/app.routing';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(FormsModule),
-    provideHttpClient(),
-    provideRouter(appRoutes)
+    provideHttpClient()
   ]
 }).catch((err) => console.error(err));

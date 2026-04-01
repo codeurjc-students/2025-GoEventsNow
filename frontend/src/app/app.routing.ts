e056eb1c27076cc27fa53a107ad9049985b48b7b
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2026-04-01 14:36:50 +0200

    feat: add registration component and routing

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { EventListComponent } from './components/events/event-list.component';
 import { ErrorComponent } from './components/error/error.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const appRoutes: Routes = [
 
     { path: '', component: EventListComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: 'error/:type', component: ErrorComponent },
     { path: '**', redirectTo: 'error/404' }
 

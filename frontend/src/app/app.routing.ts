05c06d9a1e892c35471729f936285aeaa0704426
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2026-04-01 14:16:36 +0200

    feat: add login component and routing

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 
 import { EventListComponent } from './components/events/event-list.component';
 import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const appRoutes: Routes = [
 
     { path: '', component: EventListComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'error/:type', component: ErrorComponent },
     { path: '**', redirectTo: 'error/404' }
 

332d9fc12adea5818c3d790ce08f243a22453685
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2026-04-08 20:19:41 +0200

    feat: adapt ticket selection component in order to add the new attributes and  dynamic pricing and availability for basic and VIP tickets

--- frontend/src/app/components/tickets/ticket-selection.component.ts
+++ frontend/src/app/components/tickets/ticket-selection.component.ts
@@ -9,24 +9,29 @@ import { UserService } from "../../service/user.service";
 import { Ticket } from "../../model/ticket";
 import { TicketService } from "../../service/ticket.service";
 import { NgbAlert } from "@ng-bootstrap/ng-bootstrap/alert";
-
+import { FormsModule } from "@angular/forms";
 
 @Component({
     standalone: true,
     selector: 'app-ticket-selection',
     templateUrl: './ticket-selection.component.html',
-    imports: [CommonModule, NgbAlert]
+    imports: [CommonModule, NgbAlert, FormsModule]
 })
 
 export class TicketSelectionComponent implements OnInit {
 
+
+    selectedTicketType: string = 'BASIC';
     quantity: number = 0;
     event$: Observable<Event> = new Observable<Event>();
     eventId: number = 0;
     user: User = {} as User;
     ticket: Ticket = {} as Ticket;
     ticketUser: Ticket[] = [];
-    priceBasic: number = 89.99;
+    priceBasic: number = 0;
+    priceVip: number = 0;
+    basicAvailable: number = 0;
+    vipAvailable: number = 0;
     priceTotal: number = 0;
     errorMessage: string | null = null;
 
@@ -36,12 +41,18 @@ export class TicketSelectionComponent implements OnInit {
     ngOnInit(): void {
         const id = this.activatedRoute.snapshot.params['id'];
         this.event$ = this.eventService.findById(id);
-        
+
         if (id) {
             this.eventId = Number(id);
+            this.event$.subscribe(event => {
+                this.priceBasic = event.basicPrice;
+                this.priceVip = event.vipPrice;
+                this.basicAvailable = event.availableBasicTickets;
+                this.vipAvailable = event.availableVipTickets;
+            });
             this.ticket = {
                 ticketType: '',
-                price: this.priceBasic,
+                price: 0,
                 numTickets: 0,
                 eventId: 0,
                 userOwnerId: 0
@@ -55,11 +66,11 @@ export class TicketSelectionComponent implements OnInit {
 
     }
 
+
     increase(): void {
         this.quantity += 1;
         this.reloadPriceTotal();
     }
-
 
     decrease(): void {
         if (this.quantity > 0) {
@@ -75,11 +86,23 @@ export class TicketSelectionComponent implements OnInit {
             return;
         }
 
+        if (this.selectedTicketType === 'BASIC' && this.quantity > this.basicAvailable) {
+            this.errorMessage = 'Not enough basic tickets available.';
+            return;
+        }
+
+        if (this.selectedTicketType === 'VIP' && this.quantity > this.vipAvailable) {
+            this.errorMessage = 'Not enough VIP tickets available.';
+            return;
+        }
+
+
         this.errorMessage = null;
 
-        this.ticket.eventId = this.eventId
-        this.ticket.userOwnerId = this.user.id
-        this.ticket.numTickets = this.quantity
+        this.ticket.eventId = this.eventId;
+        this.ticket.userOwnerId = this.user.id;
+        this.ticket.numTickets = this.quantity;
+        this.ticket.ticketType = this.selectedTicketType;
 
         this.reloadPriceTotal();
 
@@ -90,6 +113,18 @@ export class TicketSelectionComponent implements OnInit {
                 this.ticket = ticket
                 this.user.tickets?.push(ticket)
                 this.user.numTicketsBought = this.user.numTicketsBought + ticket.numTickets
+                this.event$.subscribe(event => {
+                    if (ticket.ticketType === 'BASIC') {
+                        event.availableBasicTickets = event.availableBasicTickets - ticket.numTickets;
+                    } else if (ticket.ticketType === 'VIP') {
+                        event.availableVipTickets = event.availableVipTickets - ticket.numTickets;
+                    }
+                    this.eventService.createOrReplaceEvent(event).subscribe({
+                        next: () => {
+                            this.event$ = this.eventService.findById(this.eventId);
+                        }
+                    });
+                });
 
                 this.userService.replaceUser(this.user).subscribe({
                     next: (updatedUser: User) => {
@@ -103,6 +138,10 @@ export class TicketSelectionComponent implements OnInit {
     }
 
     reloadPriceTotal(): void {
-        this.priceTotal = this.priceBasic * this.quantity;
+        if (this.selectedTicketType === 'BASIC') {
+            this.priceTotal = this.priceBasic * this.quantity;
+        } else {
+            this.priceTotal = this.priceVip * this.quantity;
+        }
     }
 }

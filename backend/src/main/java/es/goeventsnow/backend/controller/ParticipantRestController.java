dba979760db6b0c82596c0cc346605aeeeb96c74
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2026-04-27 10:53:34 +0200

    refactor: clean up imports and standardize exception handling in ParticipantService

--- backend/src/main/java/es/goeventsnow/backend/controller/ParticipantRestController.java
+++ backend/src/main/java/es/goeventsnow/backend/controller/ParticipantRestController.java
@@ -3,7 +3,6 @@ package es.goeventsnow.backend.controller;
 import java.io.IOException;
 import java.net.URI;
 import java.sql.SQLException;
-import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -19,8 +18,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;

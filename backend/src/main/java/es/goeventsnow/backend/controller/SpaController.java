0a7894a2ce6ccecc17e48c094efc1bbc00ae9796
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2026-05-12 18:36:38 +0200

    fix: update SpaController redirect to forward to index.html for proper SPA routing

--- backend/src/main/java/es/goeventsnow/backend/controller/SpaController.java
+++ backend/src/main/java/es/goeventsnow/backend/controller/SpaController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SpaController {
 
-    @GetMapping("/{path:[^\\.]*}")
+    @GetMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
-        return "forward:/";
+        return "forward:/index.html";
     }
 }

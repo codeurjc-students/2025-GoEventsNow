1cb44899a1f2f0733958d221b8156b44d7b62de5
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2026-05-31 21:13:20 +0200

    Add reviews support to Event model, DTO, and mapper

--- backend/src/main/java/es/goeventsnow/backend/dto/event/EventMapper.java
+++ backend/src/main/java/es/goeventsnow/backend/dto/event/EventMapper.java
@@ -7,19 +7,23 @@ import java.util.List;
 import es.goeventsnow.backend.model.Event;
 import es.goeventsnow.backend.dto.ticket.TicketMapper;
 import es.goeventsnow.backend.dto.participant.ParticipantMapper;
+import es.goeventsnow.backend.dto.review.ReviewMapper;
+
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring", uses = {TicketMapper.class, ParticipantMapper.class}, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel = "spring", uses = {TicketMapper.class, ParticipantMapper.class, ReviewMapper.class}, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface EventMapper {
 
     @Mapping(target = "participants", source = "participants")
     @Mapping(target = "tickets", source = "tickets")
+    @Mapping(target = "reviews", source = "reviews")
     EventDTO toDTO(Event event);
 
     List<EventDTO> toDTOs(Collection<Event> events);
 
     @Mapping(target = "participants", source = "participants")
     @Mapping(target = "tickets", source = "tickets")
+    @Mapping(target = "reviews", source = "reviews")
     Event toDomain(EventDTO eventDTO);
     
 }
